9309ce61fcc4a5e3be950d6227e9104b434da513
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-02-01 17:27:28 +0100

    New Nodde

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * struct binary_tree_node - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+
+struct binary_tree_node
+{
+    int n;
+    struct binary_tree_node *parent;
+    struct binary_tree_node *left;
+    struct binary_tree_node *right;
+};
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return new_node;
+
+	if (new_node == NULL) {
+		return NULL;
+	}
+}
--- binary_trees.h
+++ binary_trees.h
@@ -1,7 +1,27 @@
-#ifndef _BINARY_TREE_
-#define _BINARY_TREE_
+#ifndef _BINARY_TREES_
+#define _BINARY_TREES_
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+typedef struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
+
+void binary_tree_print(const binary_tree_t *);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -23,4 +43,4 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
-#endif /* _BINARY_TREE_ */
+#endif /* _BINARY_TREES_ */
